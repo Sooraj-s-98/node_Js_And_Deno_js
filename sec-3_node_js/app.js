5c6932ef6966e5fbcd29b44b8ebe67e36f386071
sooraj <development@liiighthouse.com>
2021-05-23 18:47:29 +0530

    Blocking and Non-Blocking Code

--- sec-3_node_js/app.js
+++ sec-3_node_js/app.js
@@ -7,24 +7,27 @@ const server = http.createServer((req, res) => {
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write('</html>');
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
-    req.on('data', (chunk) => {
+    req.on('data', chunk => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
