3edbbc21cb957f75ff86c778d1a2297e0302bd02
sooraj <development@liiighthouse.com>
2021-05-23 18:29:58 +0530

    Parsing Request Bodies

--- sec-3_node_js/app.js
+++ sec-3_node_js/app.js
@@ -12,7 +12,16 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
