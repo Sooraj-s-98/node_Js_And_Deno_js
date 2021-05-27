633d444c76803671ee72b84e2121a5fbc8524b40
sooraj <development@liiighthouse.com>
2021-05-27 08:46:47 +0530

    adding middleware

--- sec-3_node_js/app.js
+++ sec-3_node_js/app.js
@@ -1,39 +1,19 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/users') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  // Send a HTML response with some "Page not found text
-  if (url === '/create-user') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
-  }
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
 });
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
