fae2bff5844435b276e2e6ab62d045864e03686d
sooraj <development@liiighthouse.com>
2021-05-27 08:37:38 +0530

    node js basic

--- sec-3_node_js/app.js
+++ sec-3_node_js/app.js
@@ -1,40 +1,39 @@
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  const method = req.method;
   if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
+    res.write('<head><title>Assignment 1</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
   }
-  if (url === '/message' && method === 'POST') {
+  if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  // Send a HTML response with some "Page not found text
+  if (url === '/create-user') {
     const body = [];
     req.on('data', chunk => {
-      console.log(chunk);
       body.push(chunk);
     });
-    return req.on('end', () => {
+    req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
+      console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
     });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
 });
 
 server.listen(3000);
