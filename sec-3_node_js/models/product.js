b5915a0ceef610ca7e2705d938f94faa92816c17
sooraj <development@liiighthouse.com>
2021-05-29 10:49:46 +0530

    Storing and Fetching Data from Files Via the Model

--- sec-3_node_js/models/product.js
+++ sec-3_node_js/models/product.js
@@ -1,15 +1,37 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
