61ec4eaae7cfb8eea674bc3fedc75edd255f90d3
sooraj <development@liiighthouse.com>
2021-05-31 09:28:13 +0530

    Deleting Cart Items And product

--- sec-3_node_js/controllers/admin.js
+++ sec-3_node_js/controllers/admin.js
@@ -63,3 +63,9 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+};
