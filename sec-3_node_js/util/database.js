785ea5b1c7fed9858a397a2957f9989bbb25eec6
sooraj s <sooraj.s@ibilglobal.conm>
2021-06-09 10:03:37 +0530

    Understanding Sequelize

--- sec-3_node_js/util/database.js
+++ sec-3_node_js/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
